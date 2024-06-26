b82cc255948b25afa2546e5b9c72962528b821e4
Egor Quenum <georgequenum@yahoo.com>
2024-06-26 21:54:14 +0400

    Add and wire up test monitor configuration

--- WebPageChangeMonitor.Api/Program.cs
+++ WebPageChangeMonitor.Api/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebPageChangeMonitor.Models.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<ChangeMonitorOptions>(
+    builder.Configuration.GetSection(ChangeMonitorOptions.SectionName));
 
 var app = builder.Build();
 
