4fce64330f29c612a9ef9b95ab11a2941054797a
Egor Quenum <georgequenum@yahoo.com>
2024-06-26 21:54:38 +0400

    Add a temporary controller to test the configs

---
+++ WebPageChangeMonitor.Api/Controllers/ConfigurationsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using WebPageChangeMonitor.Models.Options;
+
+namespace WebPageChangeMonitor.Api;
+
+[ApiController]
+[Route("api/public/configs")]
+public class ConfigurationsController : ControllerBase
+{
+    private readonly ChangeMonitorOptions _options;
+
+    public ConfigurationsController(IOptions<ChangeMonitorOptions> options)
+    {
+        _options = options.Value;
+    }
+
+    public IActionResult Get()
+    {
+        return Ok(_options);
+    }
+}
