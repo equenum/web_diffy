a756d96152e5bc84c805277943af552ed1add166
Egor Quenum <georgequenum@yahoo.com>
2024-07-13 00:59:49 +0400

    Extend htm parser to support target context

--- WebPageChangeMonitor.Services/Parsers/HtmlParser.cs
+++ WebPageChangeMonitor.Services/Parsers/HtmlParser.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Linq;
 using HtmlAgilityPack;
+using WebPageChangeMonitor.Models.Change;
 
 namespace WebPageChangeMonitor.Services.Parsers;
 
 public class HtmlParser : IHtmlParser
 {
-    public string GetNodeInnerText(string html)
+    public string GetNodeInnerText(string html, TargetContext context)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(html, nameof(html));
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
 
         var document = new HtmlDocument();
         document.LoadHtml(html);
@@ -17,7 +19,8 @@ public class HtmlParser : IHtmlParser
         //  - https://developer.mozilla.org/en-US/docs/Web/XPath
         //  - https://devhints.io/xpath
 
-        var xPath = "//span[contains(@class, 'p-name vcard-fullname d-block overflow-hidden')]";
+        // var xPath = "//span[contains(@class, 'p-name vcard-fullname d-block overflow-hidden')]";
+        var xPath = $"//{context.HtmlTag}[contains(@{context.Selector.Type}, '{context.Selector.Value}')]".ToLowerInvariant();
         var targetNode = document.DocumentNode
             .SelectNodes(xPath)
             .FirstOrDefault();
--- WebPageChangeMonitor.Services/Parsers/IHtmlParser.cs
+++ WebPageChangeMonitor.Services/Parsers/IHtmlParser.cs
@@ -1,6 +1,8 @@
-﻿namespace WebPageChangeMonitor.Services.Parsers;
+﻿using WebPageChangeMonitor.Models.Change;
+
+namespace WebPageChangeMonitor.Services.Parsers;
 
 public interface IHtmlParser
 {
-    string GetNodeInnerText(string html);
+    string GetNodeInnerText(string html, TargetContext context);
 }
