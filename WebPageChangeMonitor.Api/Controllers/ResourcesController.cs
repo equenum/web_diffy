3a060ae4e99e7058eb67f17d22ed23b55663f78b
Egor Quenum <georgequenum@yahoo.com>
2024-07-14 00:45:01 +0400

    Replace config controller with one for resources

---
+++ WebPageChangeMonitor.Api/Controllers/ResourcesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebPageChangeMonitor.Api.Controllers;
+
+[ApiController]
+[Route("api/public/resources")]
+public class ResourcesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Resources!");
+    }
+}
