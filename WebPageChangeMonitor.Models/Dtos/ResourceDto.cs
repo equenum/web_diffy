e96aa61ba6b40a82a0365be878c80b4a13afa5ec
Egor Quenum <georgequenum@yahoo.com>
2024-07-18 21:28:42 +0400

    Introduce dtos

---
+++ WebPageChangeMonitor.Models/Dtos/ResourceDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WebPageChangeMonitor.Models.Dtos;
+
+public class ResourceDto
+{
+    public Guid Id { get; set; }
+    public string DisplayName { get; set; }
+    public string Description { get; set; }
+}
