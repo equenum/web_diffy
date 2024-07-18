0ebda5cbb34202bd33f4be6264b157fde363f7c8
Egor Quenum <georgequenum@yahoo.com>
2024-07-18 21:09:20 +0400

    Add WebPageChangeMonitor.Common

---
+++ WebPageChangeMonitor.Common/Attributes/QuartzCronExpressionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Quartz;
+
+namespace WebPageChangeMonitor.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+public class QuartzCronExpressionAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value,
+        ValidationContext validationContext)
+    {
+        if (value is string expression)
+        {
+            if (!CronExpression.IsValidExpression(expression))
+            {
+                return new ValidationResult("Invalid Quartz cron expression format!");
+            }
+
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult("Invalid Quartz cron expression value type. Expected - 'string'.");
+    }
+}
