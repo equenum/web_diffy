e606aa4f5b302aad58fd1f0b71936d597b68c9e7
Egor Quenum <georgequenum@yahoo.com>
2024-07-18 21:39:50 +0400

    Add Mapperly mappers

---
+++ WebPageChangeMonitor.Api/Infrastructure/Mappers/ResourceMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebPageChangeMonitor.Models.Domain;
+using WebPageChangeMonitor.Models.Dtos;
+using WebPageChangeMonitor.Models.Entities;
+
+namespace WebPageChangeMonitor.Api.Infrastructure.Mappers;
+
+[Mapper]
+public static partial class ResourceMapper
+{
+    public static partial ResourceEntity ToResourceEntity(this Resource resource);
+    public static partial ResourceDto ToResourceDto(this ResourceEntity resourceEntity);
+}
