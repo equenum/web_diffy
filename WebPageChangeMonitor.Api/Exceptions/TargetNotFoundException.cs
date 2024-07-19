76d3d6efc6200fa8f7ae32994b8f505bf1428c0e
Egor Quenum <georgequenum@yahoo.com>
2024-07-19 23:23:48 +0400

    Add target exceptions; add target mapper

---
+++ WebPageChangeMonitor.Api/Exceptions/TargetNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WebPageChangeMonitor.Api.Exceptions;
+
+public class TargetNotFoundException : Exception
+{
+    public TargetNotFoundException() { }
+
+    public TargetNotFoundException(string message)
+        : base(message)
+    { }
+
+    public TargetNotFoundException(string message, Exception inner)
+        : base(message, inner)
+    { }
+}
---
+++ WebPageChangeMonitor.Api/Infrastructure/Mappers/TargetMapper.cs
@@ -0,0 +1,13 @@
+﻿using Riok.Mapperly.Abstractions;
+using WebPageChangeMonitor.Models.Domain;
+using WebPageChangeMonitor.Models.Dtos;
+using WebPageChangeMonitor.Models.Entities;
+
+namespace WebPageChangeMonitor.Api.Infrastructure.Mappers;
+
+[Mapper]
+public static partial class TargetMapper
+{
+    public static partial TargetEntity ToTargetEntity(this Target target);
+    public static partial TargetDto ToTargetDto(this TargetEntity targetEntity);
+}
