8d5f89301261088a84afd434378e461922a8b09a
Egor Quenum <georgequenum@yahoo.com>
2024-07-24 19:45:22 +0400

    Update snapshot table schema

--- database/04_create_target_snapshots_table.sql
+++ database/04_create_target_snapshots_table.sql
@@ -5,6 +5,8 @@ create table if not exists monitor.target_snapshots (
     id uuid primary key,
     target_id uuid references monitor.targets(id),
     value text not null,
+    is_expected_value boolean default false not null,
+    is_change_detected boolean default false not null,
     created_at timestamp default statement_timestamp() not null,
     updated_at timestamp
 );
