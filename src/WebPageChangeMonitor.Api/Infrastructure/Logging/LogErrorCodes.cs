52c9fab694383e3f833c880dce38752778e64c13
Egor Quenum <georgequenum@yahoo.com>
2025-06-21 11:07:48 +0400

    Add error code logging

---
+++ src/WebPageChangeMonitor.Api/Infrastructure/Logging/LogErrorCodes.cs
@@ -0,0 +1,7 @@
+namespace WebPageChangeMonitor.Api.Infrastructure.Logging;
+
+public static class LogErrorCodes
+{
+    public const int ChangeDetectionFailed = 1;
+    public const int HtmlContentFetchFailed = 2;
+}
--- src/WebPageChangeMonitor.Api/Services/ChangeDetector.cs
+++ src/WebPageChangeMonitor.Api/Services/ChangeDetector.cs
@@ -1,5 +1,8 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using WebPageChangeMonitor.Api.Infrastructure.Logging;
 using WebPageChangeMonitor.Models.Domain;
 using WebPageChangeMonitor.Services.Detection.Strategies;
 
@@ -9,29 +12,51 @@ public class ChangeDetector : IChangeDetector
 {
     private readonly IHttpClientFactory _clientFactory;
     private readonly IChangeDetectionStrategyFactory _strategyFactory;
+    private readonly ILogger<ChangeDetector> _logger;
 
     public ChangeDetector(
-        IHttpClientFactory clientFactory, 
-        IChangeDetectionStrategyFactory strategyFactory)
+        IHttpClientFactory clientFactory,
+        IChangeDetectionStrategyFactory strategyFactory,
+        ILogger<ChangeDetector> logger)
 
     {
         _clientFactory = clientFactory;
         _strategyFactory = strategyFactory;
+        _logger = logger;
     }
 
     public async Task ProcessAsync(TargetContext context)
     {
-        var requestMessage = new HttpRequestMessage(HttpMethod.Get, context.Url);
+        var message = new HttpRequestMessage(HttpMethod.Get, context.Url);
 
         var client = _clientFactory.CreateClient();
-        var httpResponseMessage = await client.SendAsync(requestMessage);
+        var response = await client.SendAsync(message);
 
-        if (httpResponseMessage.IsSuccessStatusCode)
+        if (response.IsSuccessStatusCode)
         {
-            var html = await httpResponseMessage.Content.ReadAsStringAsync();
+            try
+            {
+                var html = await response.Content.ReadAsStringAsync();
 
-            var strategy = _strategyFactory.Get(context.ChangeType);
-            await strategy.ExecuteAsync(html, context);
+                var strategy = _strategyFactory.Get(context.ChangeType);
+                await strategy.ExecuteAsync(html, context);
+            }
+            catch (Exception)
+            {
+                _logger.LogError("Err-{ErrorCode}: Failed to process change detection for context id '{ContextId}', url '{Url}'.",
+                    LogErrorCodes.ChangeDetectionFailed,
+                    context.Id,
+                    context.Url);
+
+                throw;
+            }
+
+            return;
         }
+
+        _logger.LogError("Err-{ErrorCode}: Failed to fetch html page contents for context id '{ContextId}', url '{Url}'.",
+            LogErrorCodes.HtmlContentFetchFailed,
+            context.Id,
+            context.Url);
     }
 }
