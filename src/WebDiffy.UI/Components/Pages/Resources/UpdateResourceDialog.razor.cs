8f09567e40d73a1b64d5d1d70cbccaaf5db54908
Egor Quenum <georgequenum@yahoo.com>
2025-07-01 00:32:50 +0400

    Add resource update dialog

---
+++ src/WebDiffy.UI/Components/Pages/Resources/UpdateResourceDialog.razor.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using WebDiffy.UI.Services;
+using WebPageChangeMonitor.Models.Dtos;
+
+namespace WebDiffy.UI.Components.Pages.Resources;
+
+public partial class UpdateResourceDialog
+{
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; }
+
+    [Inject]
+    private ISnackbar Snackbar { get; set; }
+
+    [Inject]
+    private IResourceService ResourceService { get; set; }
+
+    [Parameter]
+    public ResourceDto Resource { get; set; }
+
+    private void Cancel() => MudDialog.Cancel();
+
+    private async Task UpdateResourceAsync()
+    {
+        Guid? updatedResourceId = null;
+
+        try
+        {
+            var updatedResource = await ResourceService.UpdateAsync(Resource);
+            updatedResourceId = updatedResource.Id;
+
+            Snackbar.Add("Resource updated", Severity.Success);
+        }
+        catch
+        {
+            Snackbar.Add("Operation 'Update Resource' failed", Severity.Error);
+        }
+
+        MudDialog.Close(DialogResult.Ok(updatedResourceId));
+    }
+}
