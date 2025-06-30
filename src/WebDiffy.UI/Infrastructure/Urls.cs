a230f89972fad33ff6bb1f4e3a4da6e7461a32bf
Egor Quenum <georgequenum@yahoo.com>
2025-06-30 20:12:31 +0400

    Extract routes into a separate file

---
+++ src/WebDiffy.UI/Infrastructure/Urls.cs
@@ -0,0 +1,10 @@
+namespace WebDiffy.UI.Infrastructure;
+
+public static class Urls
+{
+    public const string Home = "/";
+    public const string Resources = "resources";
+    public const string Targets = "targets";
+    public const string Snapshots = "snapshots";
+    public const string Settings = "settings";
+}
