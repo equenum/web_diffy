65ddb344b7d0306f5999fbefd35a3825d3aaae5f
Egor Quenum <georgequenum@yahoo.com>
2025-07-02 02:31:46 +0400

    Add user settings for item guids

--- src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
+++ src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
@@ -3,11 +3,17 @@ namespace WebDiffy.UI.Infrastructure;
 public static class UserAppSettings
 {
     public static bool AreDeletePopUpsEnabled { get; set; } = true;
+    public static bool AreItemIdColumnsEnabled { get; set; } = true;
+    public static bool AreItemIdsShortened { get; set; } = true;
+    public static int ItemIdSubSectionLength { get; set; } = 12;
     public static int[] GridPageSizeOptions { get; set; } = [10, 25, 50, 100];
 
     public static void Reset()
     {
         AreDeletePopUpsEnabled = true;
+        AreItemIdColumnsEnabled = true;
+        AreItemIdsShortened = true;
+        ItemIdSubSectionLength = 12;
         GridPageSizeOptions = [10, 25, 50, 100];
     }
 }
