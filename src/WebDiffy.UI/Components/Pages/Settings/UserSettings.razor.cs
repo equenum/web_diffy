72081d6faec3aba50a0873da907804dc7f9db51b
Egor Quenum <georgequenum@yahoo.com>
2025-07-01 22:45:10 +0400

    Add grid pager user option

--- src/WebDiffy.UI/Components/Pages/Settings/UserSettings.razor.cs
+++ src/WebDiffy.UI/Components/Pages/Settings/UserSettings.razor.cs
@@ -1,6 +1,24 @@
+using System.Linq;
+using WebDiffy.UI.Infrastructure;
+
 namespace WebDiffy.UI.Components.Pages.Settings;
 
 public partial class UserSettings
 {
+    private const string ArrayDelimeter = ", ";
 
+    private static string ConvertArrayBasedValue<T>(T[] items) where T : struct
+    {
+        return string.Join(ArrayDelimeter, items.Select(item => item.ToString()));
+    }
+
+    private static void UpdateGridPageSizeOptions(string value)
+    {
+        UserAppSettings.GridPageSizeOptions = [.. value.Split(ArrayDelimeter).Select(int.Parse)];
+    }
+
+    private static void Reset()
+    {
+        UserAppSettings.Reset();
+    }
 }
--- src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
+++ src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
@@ -3,4 +3,11 @@ namespace WebDiffy.UI.Infrastructure;
 public static class UserAppSettings
 {
     public static bool AreDeletePopUpsEnabled { get; set; } = true;
+    public static int[] GridPageSizeOptions { get; set; } = [10, 25, 50, 100];
+
+    public static void Reset()
+    {
+        AreDeletePopUpsEnabled = true;
+        GridPageSizeOptions = [10, 25, 50, 100];
+    }
 }
