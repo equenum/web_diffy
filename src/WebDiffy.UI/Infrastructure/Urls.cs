8dc8ffc6bf0cd8bf1681b258f6723317ec12b93b
Egor Quenum <georgequenum@yahoo.com>
2025-07-11 02:38:16 +0400

    Add Dashboard nav panel link

--- src/WebDiffy.UI/Infrastructure/Urls.cs
+++ src/WebDiffy.UI/Infrastructure/Urls.cs
@@ -3,8 +3,8 @@ namespace WebDiffy.UI.Infrastructure;
 public static class Urls
 {
     public const string Home = "/";
+    public const string Dashboard = "dashboard";
     public const string Resources = "resources";
     public const string Targets = "targets";
-    public const string Snapshots = "snapshots";
     public const string Settings = "settings";
 }
