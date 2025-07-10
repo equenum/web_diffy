3a0a631ac9af28cea8fe67cd9ff3391604ae3072
Egor Quenum <georgequenum@yahoo.com>
2025-07-11 02:40:22 +0400

    Add Dashboard page component

---
+++ src/WebDiffy.UI/Components/Pages/Dashboard/DashboardPage.razor.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+
+namespace WebDiffy.UI.Components.Pages.Dashboard;
+
+public partial class DashboardPage
+{
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+    }
+}
