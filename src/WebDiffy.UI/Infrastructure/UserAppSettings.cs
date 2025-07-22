79adb07190b08a59ae623b828d8c3d1e173838f5
Egor Quenum <georgequenum@yahoo.com>
2025-07-22 21:27:11 +0400

    Add user setting to expand panels by default

--- src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
+++ src/WebDiffy.UI/Infrastructure/UserAppSettings.cs
@@ -5,6 +5,7 @@ public static class UserAppSettings
     public static bool AreDeletePopUpsEnabled { get; set; } = true;
     public static bool AreItemIdColumnsEnabled { get; set; } = true;
     public static bool AreItemIdsShortened { get; set; } = true;
+    public static bool AreCollapsePanelsExpanded { get; set; } = false;
     public static int ItemIdSubSectionLength { get; set; } = 12;
     public static int[] GridPageSizeOptions { get; set; } = [10, 25, 50, 100];
 
@@ -13,6 +14,7 @@ public static class UserAppSettings
         AreDeletePopUpsEnabled = true;
         AreItemIdColumnsEnabled = true;
         AreItemIdsShortened = true;
+        AreCollapsePanelsExpanded = false;
         ItemIdSubSectionLength = 12;
         GridPageSizeOptions = [10, 25, 50, 100];
     }
