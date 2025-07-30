b1f84a71f4bb705807e4a1c5123a54555e44bb0b
Egor Quenum <georgequenum@yahoo.com>
2025-07-30 23:00:22 +0400

    Update db scripts

--- database/03_create_targets_table.sql
+++ database/03_create_targets_table.sql
@@ -4,6 +4,7 @@ start transaction;
 create table if not exists monitor.targets (
     id uuid primary key,
     resource_id uuid references monitor.target_resources(id) on delete cascade,
+    state text not null,
     display_name text not null,
     description text,
     url text not null,
