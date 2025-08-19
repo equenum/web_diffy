996ff9978341b0d6e77b51c634fcbbca5516d055
Egor Quenum <georgequenum@yahoo.com>
2025-08-20 03:48:05 +0400

    Update validation messages

--- src/WebPageChangeMonitor.Models/Consts/ValidationMessages.cs
+++ src/WebPageChangeMonitor.Models/Consts/ValidationMessages.cs
@@ -1,13 +1,12 @@
 namespace WebPageChangeMonitor.Models.Consts;
 
-// remove this class after enabling attribute based validation when dotnet 10 comes out
 public static class ValidationMessages
 {
     private const string MaxLengthMessage = "Max. length";
 
     public const string Required = "Required";
-    public const string Empty = "Cannot be empty or whitespace";
-    public const string CronExpression = "Invalid quartz cron expression format";
+    public const string Empty = "Cannot be empty";
+    public const string CronExpression = "Invalid cron expression";
     public const string Url = "Invalid url format";
     public const string UrlSchema = "Invalid url schema";
 
