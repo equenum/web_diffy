4701f94841a5865be4492f33c4a4fac7523dd312
Egor Quenum <georgequenum@yahoo.com>
2026-05-30 23:18:00 +0400

    API Add playwright for page rendering

--- src/WebPageChangeMonitor.Api/Services/ChangeDetector.cs
+++ src/WebPageChangeMonitor.Api/Services/ChangeDetector.cs
@@ -1,82 +1,88 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
-using UUIDNext;
+using Microsoft.Extensions.Options;
+using Microsoft.Net.Http.Headers;
+using Microsoft.Playwright;
+using WebPageChangeMonitor.Api.Exceptions;
 using WebPageChangeMonitor.Common.Stats;
-using WebPageChangeMonitor.Data;
 using WebPageChangeMonitor.Models.Consts;
 using WebPageChangeMonitor.Models.Domain;
-using WebPageChangeMonitor.Models.Entities;
 using WebPageChangeMonitor.Models.Logging;
+using WebPageChangeMonitor.Models.Options;
 using WebPageChangeMonitor.Services.Detection.Strategies;
 
 namespace WebPageChangeMonitor.Api.Services;
 
 public class ChangeDetector : IChangeDetector
 {
-    private readonly IHttpClientFactory _clientFactory;
     private readonly IChangeDetectionStrategyFactory _strategyFactory;
     private readonly ILogger<ChangeDetector> _logger;
-    private readonly IDbContextFactory<MonitorDbContext> _contextFactory;
+    private readonly ChangeMonitorOptions _options;
 
     public ChangeDetector(
-        IHttpClientFactory clientFactory,
         IChangeDetectionStrategyFactory strategyFactory,
         ILogger<ChangeDetector> logger,
-        IDbContextFactory<MonitorDbContext> contextFactory)
+        IOptions<ChangeMonitorOptions> options)
     {
-        _clientFactory = clientFactory;
         _strategyFactory = strategyFactory;
         _logger = logger;
-        _contextFactory = contextFactory;
+        _options = options.Value;
     }
 
     public async Task ProcessAsync(TargetContext context)
     {
-        var client = _clientFactory.CreateClient();
-        // add playwright
-        
-        var strategy = _strategyFactory.Get(context.ChangeType);
-
-        var isSuccess = false;
-        string errorMessage;
-
         var sw = Stopwatch.StartNew();
 
         try
         {
-            var message = new HttpRequestMessage(HttpMethod.Get, context.Url);
-            var response = await client.SendAsync(message);
-
-            if (response.IsSuccessStatusCode)
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new()
             {
-                var html = await response.Content.ReadAsStringAsync();
-                await strategy.ExecuteAsync(html, context);
+                Headless = true
+            });
 
-                isSuccess = true;
+            var browserContext = await browser.NewContextAsync(new() 
+            { 
+                UserAgent = _options.Playwright.UserAgent,
+                ExtraHTTPHeaders = BuildHttpHeaders()
+            });
 
-                sw.Stop();
-                MonitorMetrics.ChangeDetectionCount.WithLabels(MetricLabels.Success.True, context.ChangeType.ToString()).Inc();
-                MonitorMetrics.ChangeDetectionDuration.WithLabels(MetricLabels.Success.True, context.ChangeType.ToString()).Observe(sw.ElapsedMilliseconds);
+            var page = await browserContext.NewPageAsync();
+            var response = await page.GotoAsync(context.Url);
 
-                return;
+            if (response is null || !response.Ok)
+            {
+                throw new HtmlParsingException();
             }
 
-            errorMessage = "Failed to fetch html page contents";
+            var html = await page.ContentAsync();
 
+            var strategy = _strategyFactory.Get(context.ChangeType);
+            await strategy.ExecuteAsync(html, context);
+
+            sw.Stop();
+            MonitorMetrics.ChangeDetectionCount.WithLabels(MetricLabels.Success.True, context.ChangeType.ToString()).Inc();
+            MonitorMetrics.ChangeDetectionDuration.WithLabels(MetricLabels.Success.True, context.ChangeType.ToString()).Observe(sw.ElapsedMilliseconds);
+        }
+        catch (HtmlParsingException)
+        {
+            sw.Stop();
             _logger.LogError("Err-{ErrorCode}: Failed to fetch html page contents for context id '{ContextId}', url '{Url}'.",
                 LogErrorCodes.Detection.FetchFailed,
                 context.Id,
                 context.Url);
+            
+            MonitorMetrics.ChangeDetectionCount.WithLabels(MetricLabels.Success.False, context.ChangeType.ToString()).Inc();
+            MonitorMetrics.ChangeDetectionDuration.WithLabels(MetricLabels.Success.False, context.ChangeType.ToString()).Observe(sw.ElapsedMilliseconds);
+
+            throw;
         }
-        catch (Exception ex)
+        catch (Exception)
         {
             sw.Stop();
-            errorMessage = $"Failed to process {context.ChangeType} change detection: {ex.Message}";
 
             _logger.LogError("Err-{ErrorCode}: Failed to process change detection for context id '{ContextId}', url '{Url}'.",
                 LogErrorCodes.Detection.Failed,
@@ -85,32 +91,23 @@ public class ChangeDetector : IChangeDetector
 
             MonitorMetrics.ChangeDetectionCount.WithLabels(MetricLabels.Success.False, context.ChangeType.ToString()).Inc();
             MonitorMetrics.ChangeDetectionDuration.WithLabels(MetricLabels.Success.False, context.ChangeType.ToString()).Observe(sw.ElapsedMilliseconds);
-        }
 
-        if (!isSuccess)
-        {
-            using (var dbContext = _contextFactory.CreateDbContext())
-            {
-                var latestPreviousSnapshot = await dbContext.TargetSnapshots
-                    .Where(snapshot => snapshot.TargetId == context.Id)
-                    .OrderByDescending(snapshot => snapshot.CreatedAt)
-                    .FirstOrDefaultAsync();
-
-                var failureSnapshot = new TargetSnapshotEntity()
-                {
-                    Id = Uuid.NewDatabaseFriendly(Database.PostgreSql),
-                    TargetId = context.Id,
-                    Value = latestPreviousSnapshot is not null ? latestPreviousSnapshot.Value : string.Empty,
-                    NewValue = latestPreviousSnapshot is not null ? latestPreviousSnapshot.NewValue : string.Empty,
-                    IsChangeDetected = false,
-                    Outcome = Outcome.Failure,
-                    Message = errorMessage,
-                    CreatedAt = DateTime.UtcNow
-                };
-
-                dbContext.TargetSnapshots.Add(failureSnapshot);
-                await dbContext.SaveChangesAsync();
-            }
+            throw;
         }
     }
+
+    private Dictionary<string, string> BuildHttpHeaders() => new()
+    {
+        { HeaderNames.Accept, "*/*" },
+        { HeaderNames.AcceptEncoding, "gzip, deflate, br, zstd" },
+        { HeaderNames.AcceptLanguage, "en-US,en;q=0.9" },
+        { UserHintHeaderNames.Ua, BuildUaUserHint() },
+        { UserHintHeaderNames.UaMobile, _options.Playwright.IsMobile ? "?1" : "?0" },
+        { UserHintHeaderNames.UaPlatform, $"\"{_options.Playwright.Platform}\"" }
+    };
+
+    private string BuildUaUserHint() =>
+        $"\"Chromium\";v=\"{_options.Playwright.ChromiumVersion}\", " + 
+        $"\"Google Chrome\";v=\"{_options.Playwright.GoogleChromeVersion}\", " + 
+        $"\"Not/A)Brand\";v=\"{_options.Playwright.GreaseVersion}\"";
 }
